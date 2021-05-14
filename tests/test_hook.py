40fd1bca66d01a8bda67f3398a810750b3832e8a
824750130 <e824750130@qq.com>
2021-05-14 16:00:20 +0800

    :white_check_mark:Add tests

---
+++ tests/test_hook.py
@@ -0,0 +1,69 @@
+import sys
+import likeshell
+import unittest
+
+from likeshell.shell import run_cls
+from likeshell import exceptions
+
+
+class MyTask(likeshell.Main):
+    def __before__(self):
+        pass
+
+    def __after__(self):
+        pass
+
+    def task(self):
+        raise RuntimeError('run task')
+
+    def params(
+            self,
+            arg1,
+            arg2,
+            arg3,
+    ):
+        if arg1 != 'hello':
+            raise ValueError(f'arg1 != hello')
+        if arg2 != 'world':
+            raise ValueError(f'arg2 != world')
+        if arg3 != '!':
+            raise ValueError(f'arg3 != !')
+
+
+def run():
+    run_cls(MyTask, MyTask.__dict__)
+
+
+class TestBasic(unittest.TestCase):
+    def test_task(self):
+        sys.argv = ['test.py', 'task']
+        try:
+            run()
+            assert False
+        except RuntimeError as e:
+            self.assertEqual('run task', str(e))
+
+    def test_param(self):
+        sys.argv = ['test.py', 'params', 'hello', 'world', '!']
+        run()
+
+        sys.argv = ['test.py', 'params', 'hello', 'world', '?']
+        try:
+            run()
+            assert False
+        except ValueError as e:
+            self.assertEqual('arg3 != !', str(e))
+
+        sys.argv = ['test.py', 'params', 'hello']
+        try:
+            run()
+            assert False
+        except exceptions.ParameterError as e:
+            self.assertEqual('MissingParameter: arg2.', str(e))
+
+        sys.argv = ['test.py', 'params', 'hello', 'world']
+        try:
+            run()
+            assert False
+        except exceptions.ParameterError as e:
+            self.assertEqual('MissingParameter: arg3.', str(e))
