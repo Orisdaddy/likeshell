2c108b0879eaedf8a9f6dc0561565bfd58b852ab
824750130 <e824750130@qq.com>
2021-05-14 16:10:03 +0800

    :construction_worker:Add CI

--- tests/test_hook.py
+++ tests/test_hook.py
@@ -16,19 +16,6 @@ class MyTask(likeshell.Main):
     def task(self):
         raise RuntimeError('run task')
 
-    def params(
-            self,
-            arg1,
-            arg2,
-            arg3,
-    ):
-        if arg1 != 'hello':
-            raise ValueError(f'arg1 != hello')
-        if arg2 != 'world':
-            raise ValueError(f'arg2 != world')
-        if arg3 != '!':
-            raise ValueError(f'arg3 != !')
-
 
 def run():
     run_cls(MyTask, MyTask.__dict__)
@@ -42,28 +29,3 @@ class TestBasic(unittest.TestCase):
             assert False
         except RuntimeError as e:
             self.assertEqual('run task', str(e))
-
-    def test_param(self):
-        sys.argv = ['test.py', 'params', 'hello', 'world', '!']
-        run()
-
-        sys.argv = ['test.py', 'params', 'hello', 'world', '?']
-        try:
-            run()
-            assert False
-        except ValueError as e:
-            self.assertEqual('arg3 != !', str(e))
-
-        sys.argv = ['test.py', 'params', 'hello']
-        try:
-            run()
-            assert False
-        except exceptions.ParameterError as e:
-            self.assertEqual('MissingParameter: arg2.', str(e))
-
-        sys.argv = ['test.py', 'params', 'hello', 'world']
-        try:
-            run()
-            assert False
-        except exceptions.ParameterError as e:
-            self.assertEqual('MissingParameter: arg3.', str(e))
